ab711ca545dd739f42130a497426bedf360e28b0
dkocsis <david.kocsis@emarsys.com>
2019-05-16 16:16:12 +0200

    feat: basic dependency injection

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import 'document-register-element';
+import 'classlist-polyfill';
+
+const defineElement = (componentClass, dependencies = []) => {
+  const tag = componentClass.name.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
+
+  customElements.define(tag, componentClass);
+
+  dependencies.forEach(dependency => defineElement(dependency));
+};
 
 import MySelect from './components/my-select';
 import MyOption from './components/my-option';
 
-customElements.define('my-select', MySelect);
-customElements.define('my-option', MyOption);
+defineElement(MySelect, [MyOption]);
