122e3727266d7def5187219b92794fa7053d1302
dkocsis <david.kocsis@emarsys.com>
2019-09-05 01:12:19 +0200

    feat: new templating capabilities

--- src/js/libs/templater.js
+++ src/js/libs/templater.js
@@ -4,6 +4,11 @@ class Templater {
   constructor(context) {
     this._context = context;
     this._templates = [];
+    this._mutationObserver = new MutationObserver(this._context.contentChangedCallback.bind(this));
+  }
+
+  static _parseHTML(content) {
+    return new DOMParser().parseFromString(content, 'text/html').body.childNodes[0];
   }
 
   init(templates) {
@@ -48,17 +53,79 @@ class Templater {
   renderAll() {
     this._templates.forEach(template => {
       if (template.markup && template.container) {
-        render(template.container, () => template.markup.call(this._context, html));
+        render(template.container, () => {
+          if (typeof template.markup === 'function') {
+            return template.markup.call(this._context, html);
+          } else if (typeof template.markup === 'object') {
+            return this.buildFromTemplate(template.markup)();
+          }
+
+          return this.buildFromTemplate(document.querySelector(template.markup).content)();
+        });
       }
     });
   }
 
   render(templateName = '_default') {
-    return this._templates.find(template => template.name === templateName).markup.call(this._context, html);
+    const markup = this._templates.find(template => template.name === templateName).markup;
+
+    if (typeof markup === 'function') {
+      return markup.call(this._context, html);
+    } else if (typeof markup === 'object') {
+      const markupElement = markup.nodeName === 'TEMPLATE' ? markup.content : markup;
+
+      return this.buildFromTemplate(markupElement);
+    }
+
+    const markupSelector = document.querySelector(markup);
+    const markupElement = markupSelector.nodeName === 'TEMPLATE' ? markupSelector.content : markupSelector;
+
+    return this.buildFromTemplate(markupElement);
   }
 
   getContainer(templateName = '_default') {
     return this._templates.find(template => template.name === templateName).container;
+  }
+
+  buildFromTemplate(template) {
+    this._observeTemplate(template);
+
+    return render => {
+      const innerHTML = [].map.call(template.childNodes, x => x.outerHTML).join('');
+      const data = this._context._state.get() || {};
+
+      const variableRegexp = /(\$\{[\w]+\})/g
+      const templateValues = innerHTML.split(variableRegexp).reduce((values, item) => {
+        if ('$' === item[0] && '{' === item[1] && '}' === item.slice(-1)) {
+          values.keys.push(item.slice(2, -1));
+        } else {
+          values.markup.push(item);
+        }
+
+        return values;
+      }, { markup: [], keys: [] });
+
+      templateValues.id = ':' + templateValues.markup.join().trim();
+
+      const output = [
+        templateValues.markup,
+        ...templateValues.keys.map(key => data[key])
+      ];
+      output.raw = { value: templateValues.markup };
+
+      return html(...output);
+    };
+  }
+
+  _observeTemplate(template) {
+    this._mutationObserver.disconnect();
+
+    this._mutationObserver.observe(template, {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: true
+    });
   }
 
   _cleanUpContainer() {
